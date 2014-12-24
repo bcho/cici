c61b4fba878e7fff211f7a2a706d8fb8bc9385fa
hbc <bcxxxxxx@gmail.com>
2014-12-24 20:12:49 +0800

    Some more explainations on recur.

--- recur.clj
+++ recur.clj
@@ -5,3 +5,14 @@
     ; Step 2: bind x with new-x
     ; Step 3: go to print-down-from's start
     (recur (dec x))))
+
+
+(defn sum-down-from [initial-x]
+  (loop [sum 0, x initial-x]
+    (if (pos? x)
+      (recur (+ sum x) (dec x))   ; jump back to the nearest loop or fn method.
+      sum)))
+
+
+; Recur must stay in the tail!
+; (fn [x] (recur x) (println x))
