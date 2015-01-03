bc0f026efb14b37b7adc1d21634ebd5c38f35d0e
hbc <bcxxxxxx@gmail.com>
2015-01-03 23:06:38 +0800

    Add notes on datastructure.

--- datastructure.clj
+++ datastructure.clj
@@ -5,6 +5,10 @@
 ; Equals -> has same values & order
 (= [1 2 3] '(1 2 3))
 
+; Use conj all the time
+(conj '(2 3) 1) ; '(1 2 3)
+(conj [2 3] 1)  ; [1 2 3]
+
 ; Vector:
 ; 
 ; Good at:
@@ -32,3 +36,33 @@
 
 ; slice
 (subvec [1 2 3 4 5] 1 4)
+
+
+; List:
+;
+; Baisc data structure in lisp family language.
+;
+; - left hand operations
+; - flavor in using conj over cons
+
+
+; Set:
+
+; What are equal?
+
+#{[] ()}
+;=> #{ [] }  ; because [] and () has same value
+
+
+; pos
+
+; structure first -> convert different structure into isomorphic structure.
+(defn index [coll]
+  (cond
+    (map? coll) (seq coll)
+    (set? coll) (map vector coll coll)
+    :else (map vector (iterate inc 0) coll)))
+
+; convert all
+(defn pos [pred coll]
+  (for [[i v] (index coll) :when (pred v)] i))
